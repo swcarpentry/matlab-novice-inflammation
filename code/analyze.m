a721a087ec566df05b1cdf512ff06448f6d54147
Ashwin Srinath <ashwinsrnth@gmail.com>
2015-07-09 15:18:48 -0400

    add three versions of analyze

--- code/analyze.m
+++ code/analyze.m
@@ -1,27 +1,26 @@
-for idx = 1:3
+patient_data = csvread('data/inflammation-01.csv');
 
-    % Generate strings for file and image names:
-    file_name = sprintf('data/inflammation-%02d.csv', idx);
-    img_name = sprintf ('patient_data-%02d.png', idx);
+disp(['Maximum inflammation: ', num2str(max(patient_data(:)))]);
+disp(['Minimum inflammation: ', num2str(min(patient_data(:)))]);
+disp(['Standard deviation: ', num2str(std(patient_data(:)))]);
 
-    patient_data = csvread(file_name);
-    ave_inflammation = mean(patient_data, 1);
+ave_inflammation = mean(patient_data, 1);
 
-    figure()
+figure('visible', 'off')
 
-    subplot(2, 2, 1);
-    plot(ave_inflammation);
-    ylabel('average')
+subplot(1, 3, 1);
+plot(ave_inflammation);
+ylabel('average')
 
-    subplot(2, 2, 2);
-    plot(max(patient_data, [], 1));
-    ylabel('max')
+subplot(1, 3, 2);
+plot(max(patient_data, [], 1));
+ylabel('max')
 
-    subplot(2, 2, 3);
-    plot(min(patient_data, [], 1));
-    ylabel('min')
+subplot(1, 3, 3);
+plot(min(patient_data, [], 1));
+ylabel('min')
 
-    print('-dpng', img_name);
-    close();
+% save plot to disk as png image:
+print -dpng 'patient_data-01.png'
 
-end
+close()
---
+++ code/analyze_plot_switch.m
@@ -0,0 +1,40 @@
+
+% plot_switch:
+%   0 - show plots interactively
+%   1 - save plots to disk
+
+plot_switch = 0;
+
+for idx = 1:3
+
+    % Generate strings for file and image names:
+    file_name = sprintf('data/inflammation-%02d.csv', idx);
+    img_name = sprintf ('patient_data-%02d.png', idx);
+
+    patient_data = csvread(file_name);
+    ave_inflammation = mean(patient_data, 1);
+
+    if plot_switch == 1
+        figure('visible', 'off')
+    else
+        figure('visible', 'on')
+    end
+
+    subplot(2, 2, 1);
+    plot(ave_inflammation);
+    ylabel('average')
+
+    subplot(2, 2, 2);
+    plot(max(patient_data, [], 1));
+    ylabel('max')
+
+    subplot(2, 2, 3);
+    plot(min(patient_data, [], 1));
+    ylabel('min')
+
+    if plot_switch == 1
+        print('-dpng', img_name);
+        close()
+    end
+
+end
