cb238944d4530c569634788c93ce15f023ab4050
Gerard Capes <gerardcapes@gmail.com>
2016-02-10 14:01:54 +0000

    Following disccusion with @ashwinsrnth, more explanation added re:isnan, and moved solutions to instructor guide.
    Code files:
    range_overlap
    	Removed solution to exercise.
    
    Lesson files:
    06-defensive.md
    	Updated test_range_overlap to match modified matlab file.
    	Added some introductory explanation for isequal and isnan functions.

--- code/test_range_overlap.m
+++ code/test_range_overlap.m
@@ -1,6 +1,5 @@
 %script test_range_overlap.m
 
-
 assert(isnan(range_overlap([0.0, 1.0], [5.0, 6.0]))==1);
 assert(isnan(range_overlap([0.0, 1.0], [1.0, 2.0]))==1);
 assert(isequal(range_overlap([0, 1.0]),[0, 1.0]));
